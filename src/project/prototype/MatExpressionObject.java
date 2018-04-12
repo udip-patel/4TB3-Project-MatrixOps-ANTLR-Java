ed6f9eef91711e18d9245bbc25aabb4770257647
Udip Patel <patel.udip@gmail.com>
2018-04-12 01:19:53 -0400

    replaced name of attribute to be more fitting

--- src/project/prototype/MatExpressionObject.java
+++ src/project/prototype/MatExpressionObject.java
@@ -3,8 +3,8 @@ package project.prototype;
 import java.util.*;
 
 public class MatExpressionObject {
-    //TYPE refers to whether this expr is a matrix (1) or a scalar value (0)
-    public boolean type;
+    //type, refers to whether this expr is a matrix (1) or a scalar value (0)
+    public boolean isMatrix;
     public double scalarValue;
     //uninitialized array, best to leave uninit. until calculations are needed
     public double[][] matrix;
@@ -12,17 +12,17 @@ public class MatExpressionObject {
 
     //base constructor --> set up an expression of type matrix
     public MatExpressionObject(){
-        this.type = true;
+        this.isMatrix = true;
     }
     //constructor used for setting up an expr that returns a scalar value
     public MatExpressionObject(double scalarValue){
-        this.type = false;
+        this.isMatrix = false;
         this.scalarValue = scalarValue;
     }
 
     //constructor used to load/copy a representation of a matrix
     public MatExpressionObject(double[][] matrix){
-        this.type = true;
+        this.isMatrix = true;
         this.matrix = new double[matrix.length][matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
             this.matrix[i] = matrix[i].clone();
